7c329810959bf00aa16bb4a5fb2936ee2a9bee92
系統服務 <52878930+asst168@users.noreply.github.com>
2019-07-27 15:23:02 +0800

    Add files via upload

---
+++ doc/menu_items.js
@@ -0,0 +1,22 @@
+/*
+  --- menu items --- 
+  note that this structure has changed its format since previous version.
+  additional third parameter is added for item scope settings.
+  Now this structure is compatible with Tigra Menu GOLD.
+  Format description can be found in product documentation.
+*/
+var MENU_ITEMS = [
+	['新系統導入建議','first.htm',{'tw' : 'content'}],
+	['如何進出本系統','io.htm',{'tw' : 'content'}],
+	['主畫面的螢幕說明','screen.htm',{'tw' : 'content'}],
+	['識別顏色','color.htm',{'tw' : 'content'}],
+	['輔助視窗的使用','f1.htm',{'tw' : 'content'}],
+	['單筆資料維護','single.htm',{'tw' : 'content'}],
+	['多筆資料維護','multi.htm',{'tw' : 'content'}],
+	['列印','print.htm',{'tw' : 'content'}],
+	['多筆貨單自動結帳','money.htm',{'tw' : 'content'}],
+	['系統資料維護','system.htm',{'tw' : 'content'}],
+	['系統技術文件','tech.htm',{'tw' : 'content'}],
+	['排除系統狀況','sos.htm',{'tw' : 'content'}],
+];
+
